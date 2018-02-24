c59bf645ff5edd0e86405e3da8e5f920792c055f
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-02-24 19:16:07 +0300

    Upgrade main layout

--- source/routes/main.js
+++ source/routes/main.js
@@ -3,6 +3,6 @@ const manifest = require('../../dist/manifest.json');
 
 module.exports = (request, response) => {
   response.render('index', {
-    title: 'TopLN.ru | Test task', script: `/dist/${manifest['main.js']}`,
+    script: `/dist/${manifest['main.js']}`,
   });
 };
